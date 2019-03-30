5e836ff90671dcde3c94b6b79a2adb3df44d6655
nbiha <thisnabihah@gmail.com>
2019-03-30 16:07:50 +0800

    first commit

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,11 @@
+<ul>
+    @foreach ($posts as $post)
+    <li>
+        <a href="{{route('posts.edit',$post)}}">
+        {{ $post->title }}
+        <img width="200" src="{{ asset('storage/' .$post->image) }}" alt="">
+    </li>
+    @endforeach
+</ul>
+
+<a href="{{ route('posts.create')}}">Add post</a>
